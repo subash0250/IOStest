70977557f28315d390dd3c8fa08000f08b4d66b2
subash <gaddamsubashreddy@gmail.com>
2024-06-28 13:59:39 -0400

    project config again

---
+++ testtest/testtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtestApp.swift
+//  testtest
+//
+//  Created by Subash Gaddam on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct testtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
